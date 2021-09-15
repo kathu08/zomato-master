d3ee5dc96210e1101bde6f34ec027b78a5636291
kathu@08 <sivalingamk.19@uom.lk>
2021-09-15 12:28:41 +0530

    add google auth

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import passport from "passport";
+
+// config
+import googleAuthConfig from "./config/google.config";
 
 // microservices routes
 import Auth from "./API/Auth";
@@ -19,13 +23,18 @@ zomato.use(express.json());
 zomato.use(express.urlencoded({ extended: false }));
 zomato.use(helmet());
 zomato.use(cors());
+zomato.use(passport.initialize());
+zomato.use(passport.session());
+
+// passport configuration
+googleAuthConfig(passport);
 
 // Application Routes
 zomato.use("/auth", Auth);
 
 zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
 
-zomato.listen(3000, () =>
+zomato.listen(4000, () =>
   ConnectDB()
     .then(() => console.log("Server is running"))
     .catch(() =>
