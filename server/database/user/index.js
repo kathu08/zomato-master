5f78e14500c4b7544922dde4c09f349a85f4c0b6
kathu@08 <sivalingamk.19@uom.lk>
2021-09-15 09:36:17 +0530

    add signin route

--- server/database/user/index.js
+++ server/database/user/index.js
@@ -30,6 +30,22 @@ UserSchema.statics.findByEmailAndPhone = async ({ email, phoneNumber }) => {
   return false;
 };
 
+UserSchema.statics.findByEmailAndPassword = async ({ email, phoneNumber }) => {
+  // check whether email exist
+  const user = await UserModel.findOne({ email });
+  if (!user) throw new Error("User does not exist");
+
+  // compare password
+  const doesPasswordMatch = await bcrypt.compare(
+    password,
+    user.password
+  );
+
+  if (!doesPasswordMatch) throw new Error("Invalid Password");
+
+  return user;
+};
+
 UserSchema.pre("save", function (next) {
   const user = this;
 
