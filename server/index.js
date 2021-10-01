c03540da1f8c606f8e5e8854ea04e877ef04bec9
kathu@08 <sivalingamk.19@uom.lk>
2021-10-01 17:25:38 +0530

    add route config

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import passport from "passport";
 
 // config
 import googleAuthConfig from "./config/google.config";
+import routeConfig from "./config/route.config";
 
 // microservices routes
 import Auth from "./API/Auth";
@@ -35,6 +36,7 @@ zomato.use(passport.session());
 
 // passport configuration
 googleAuthConfig(passport);
+routeConfig(passport);
 
 // Application Routes
 zomato.use("/auth", Auth);
