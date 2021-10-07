21f299e1efe5b9ef39904284d580143ebee40d80
kathu@08 <sivalingamk.19@uom.lk>
2021-10-07 19:34:48 +0530

    add nutrition

---
+++ client/src/Components/Nutrition/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import NutritionHeroCarousal from "./NutritionHeroCarousal";
+import NutritionCarousal from "./NutritionCarousal";
+
+const Nutrition = () => {
+  return (
+    <div>
+      <NutritionHeroCarousal />
+      <div className="my-6">
+        <NutritionCarousal />
+      </div>
+    </div>
+  );
+};
+
+export default Nutrition;
