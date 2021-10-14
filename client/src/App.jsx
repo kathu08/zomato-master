8c2d4eae80a879310b8069a008114156e14cf9b6
kathu@08 <sivalingamk.19@uom.lk>
2021-10-15 00:10:29 +0530

    add menu and photos page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,8 @@ import Home from "./Page/Home";
 import Overview from "./Page/Restaurant/Overview";
 import OrderOnline from "./Page/Restaurant/OrderOnline";
 import Reviews from "./Page/Restaurant/Reviews";
+import Menu from "./Page/Restaurant/Menu";
+import Photos from "./Page/Restaurant/Photos";
 
 function App() {
   return (
@@ -32,7 +34,7 @@ function App() {
         exact
         component={OrderOnline}
       />
-      <RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={Temp} />
+      <RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={Menu} />
       <RestaurantLayoutHOC
         path="/restaurant/:id/reviews"
         exact
@@ -41,7 +43,7 @@ function App() {
       <RestaurantLayoutHOC
         path="/restaurant/:id/photos"
         exact
-        component={Temp}
+        component={Photos}
       />
     </>
   );
